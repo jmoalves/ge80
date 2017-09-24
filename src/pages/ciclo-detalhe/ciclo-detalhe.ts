ab85954ece145105f91cdc6522c31c7a9fb0c2fa
Joao Mauricio <jmauricio@pobox.com>
2017-09-24 03:54:14 -0300

    Detalhe Ciclo - Calculando totais

--- src/pages/ciclo-detalhe/ciclo-detalhe.ts
+++ src/pages/ciclo-detalhe/ciclo-detalhe.ts
@@ -26,11 +26,30 @@ export class CicloDetalhePage {
 
   subGrupo: string = "reuniao";
 
+  totalPontualidade: number = 0;
+  totalPresenca: number = 0;
+  totalVestuario: number = 0;
+  totalParticipacao: number = 0;
+  totalEspiritoEscoteiro: number = 0;
+  totalJogoTecnico: number = 0;
+  totalConquistas: number = 0;
+  totalExtras: number = 0;
+  totalPenalidade: number = 0;
+  totalAtividadeExterna: number = 0;
+
+  totalDiaReuniao: any[] = [];
+  totalDiaExtras: any[] = [];
+
+  totalGeralReuniao: number = 0;
+  totalGeralExtras: number = 0;
+
+  totalGeralMensal: number = 0;
+
   constructor(
-      public navCtrl: NavController,
-      public navParams: NavParams,
-      public patrulhaProvider: PatrulhaProvider,
-      public cicloProvider: CicloTorneioEficienciaProvider) {
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public patrulhaProvider: PatrulhaProvider,
+    public cicloProvider: CicloTorneioEficienciaProvider) {
 
     let ciclo = cicloProvider.ciclo(navParams.data.ciclo.idCiclo);
     this.ciclo = ciclo.nome;
@@ -39,8 +58,43 @@ export class CicloDetalhePage {
     this.patrulha = navParams.data.patrulha.idPatrulha;
     this.pontuacao = navParams.data.patrulha;
     this.totalPontos = cicloProvider.totalPontos(this.pontuacao);
-  }
+
+    for (let dia of this.pontuacao.pontos.porDia) {
+      this.totalPontualidade += dia.pontualidade;
+      this.totalPresenca += dia.presenca;
+      this.totalVestuario += dia.vestuario;
+      this.totalParticipacao += dia.participacao;
+      this.totalEspiritoEscoteiro += dia.espiritoEscoteiro;
+      this.totalJogoTecnico += dia.jogoTecnico;
+      this.totalConquistas += dia.conquistas;
+      this.totalExtras += dia.extras;
+      this.totalPenalidade += dia.penalidade;
+      this.totalAtividadeExterna += dia.atividadeExterna;
+
+      this.totalDiaReuniao[dia.id] =
+        dia.pontualidade +
+        dia.presenca +
+        dia.vestuario +
+        dia.participacao +
+        dia.espiritoEscoteiro +
+        dia.jogoTecnico;
+      this.totalGeralReuniao += this.totalDiaReuniao[dia.id];
+
+      this.totalDiaExtras[dia.id] =
+        dia.conquistas +
+        dia.extras +
+        dia.penalidade +
+        dia.atividadeExterna;
+      this.totalGeralExtras += this.totalDiaExtras[dia.id];
+    }
+
+    this.totalGeralMensal =
+      this.pontuacao.pontos.materialPatrulha +
+      this.pontuacao.pontos.cantoPatrulhaVirtual +
+      this.pontuacao.pontos.livrosPatrulha;
+    }
 
   ionViewDidLoad() {
   }
+
 }
