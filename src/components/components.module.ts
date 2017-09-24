3475189c09e015dde98b5d1dfcfbb8e03e63af3c
Joao Mauricio <jmauricio@pobox.com>
2017-09-24 14:41:32 -0300

    Limpeza

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
-import { IonicPageModule } from 'ionic-angular';
 
 import { ProgressBarComponent } from './progress-bar/progress-bar';
 
