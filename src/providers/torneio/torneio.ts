2913abbb8372ac224efcba20db6cb0af71c5ac46
Joao Mauricio <jmauricio@pobox.com>
2017-10-06 03:18:51 -0300

    Ordenando os dias

--- src/providers/torneio/torneio.ts
+++ src/providers/torneio/torneio.ts
@@ -204,6 +204,17 @@ export class TorneioProvider {
 
       dias.push(dia);
     }
+    dias.sort((a,b) => {
+      if (a.id < b.id)  {
+        return -1;
+      }
+
+      if (a.id > b.id) {
+        return 1;
+      }
+
+      return 0;
+    })
     patrulha.pontos.diasArray = dias;
 
     // Pontos mensais
