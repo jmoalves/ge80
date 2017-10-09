9ac08dda53a233f45b60394bfc83c38e498e9631
Joao Mauricio <jmauricio@pobox.com>
2017-10-08 22:44:55 -0300

    Textos explicativos dos itens

--- src/pages/ciclo-detalhe/ciclo-detalhe.ts
+++ src/pages/ciclo-detalhe/ciclo-detalhe.ts
@@ -68,55 +68,55 @@ export class CicloDetalhePage {
 
     switch (item) {
       case 'pontualidade':
-        msg = "Pontualidade: Pergunte aos chefes...";
+        msg = "[Pontualidade] Começa com 10 pontos. A cada elemento que chegou atrasado, perde 1 ponto.";
         break;
 
       case 'presenca':
-        msg = "Presença: Perde 1 ponto por elemento ausente. Começa com 10 pontos";
+        msg = "[Presença] Começa com 10 pontos. A cada elemento ausente, perde 1 ponto.";
         break;
 
       case 'vestuario':
-        msg = "Inspeção: Pergunte aos chefes...";
+        msg = "[Inspeção] Começa com 10 pontos. Sem bastão? Perde 4 pontos; Perde também 1 ponto para CADA falta de distintivo, caneta, lenço ou vestuário.";
         break;
 
       case 'participacao':
-        msg = "Participação:  Pergunte aos chefes...";
+        msg = "[Participação] Pergunte aos chefes...";
         break;
 
       case 'espiritoEscoteiro':
-        msg = "Espírito Escoteiro:  Pergunte aos chefes...";
+        msg = "[Espírito Escoteiro] Pergunte aos chefes...";
         break;
 
       case 'jogoTecnico':
-        msg = "Jogo Técnico:  Pergunte aos chefes...";
+        msg = "[Jogo Técnico] Pergunte aos chefes...";
         break;
 
       case 'conquistas':
-        msg = "Conquistas:  Pergunte aos chefes...";
+        msg = "[Conquistas] Especialidades: Nível 1 -> +1, Nível 2 -> +2, Nível 3 -> + 3; Progressão: +10; Insígnias especiais: +15; Cordão verde-amarelo: +20; Cordão vermelho-branco: +35; Lis de Ouro: +60";
         break;
 
       case 'extras':
-        msg = "Extras:  Pergunte aos chefes...";
+        msg = "[Extras] Palestra: +5; Boletim: +10; Promessa: +5; Quatro reuniões seguidas: +4; Ou qualquer outra coisa, a critério dos chefes.";
         break;
 
       case 'penalidade':
-        msg = "Penalidade:  Pergunte aos chefes...";
+        msg = "[Penalidade] A critério dos chefes. Fiquem espertos!";
         break;
 
       case 'atividadeExterna':
-        msg = "Atividade Externa:  Pergunte aos chefes...";
+        msg = "[Atividade Externa] Começa com 10 pontos. A cada elemento ausente, perde 1 ponto.";
         break;
 
       case 'materialPatrulha':
-        msg = "Material de Patrulha:  Pergunte aos chefes...";
+        msg = "[Material] Pergunte aos chefes...";
         break;
 
       case 'cantoPatrulhaVirtual':
-        msg = "Canto Virtual de Patrulha:  Pergunte aos chefes...";
+        msg = "[Canto Virtual]: Pergunte aos chefes...";
         break;
 
       case 'livrosPatrulha':
-        msg = "Livro de Patrulha:  Pergunte aos chefes...";
+        msg = "[Livros] Pergunte aos chefes...";
         break;
 
       default:
@@ -126,7 +126,7 @@ export class CicloDetalhePage {
 
     let toast = this.toastCtrl.create({
       message: msg,
-      duration: 3000
+      duration: 5000
     });
     toast.present();
   }
