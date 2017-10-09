d0cb75a315a16aaf719aefc6138e9d37aa7c638f
Joao Mauricio <jmauricio@pobox.com>
2017-10-08 22:19:09 -0300

    Toast - Mostra informação sobre os itens

--- src/pages/ciclo-detalhe/ciclo-detalhe.ts
+++ src/pages/ciclo-detalhe/ciclo-detalhe.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Slides, ToastController } from 'ionic-angular';
 
 import { Ciclo, PontuacaoPatrulha } from '../../models/torneio/ciclo';
 
@@ -12,7 +12,7 @@ import { PatrulhaProvider } from '../../providers/patrulha/patrulha';
   templateUrl: 'ciclo-detalhe.html',
 })
 export class CicloDetalhePage {
-  @ViewChild('slides') slides:Slides;
+  @ViewChild('slides') slides: Slides;
 
   ciclo: Ciclo;
   slideInicial: number;
@@ -21,13 +21,14 @@ export class CicloDetalhePage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
+    public toastCtrl: ToastController,
     private torneioProvider: TorneioProvider, private patrulaProvider: PatrulhaProvider) {
 
     this.ciclo = navParams.data.ciclo;
     // console.log("Ciclo[" + this.ciclo.id + "] tem " + this.ciclo.patrulhaArray.length + " patrulhas");
     for (let i in this.ciclo.patrulhaArray) {
       // console.log("Patrulha: " + i);
-      let patrulha:PontuacaoPatrulha = this.ciclo.patrulhaArray[i];
+      let patrulha: PontuacaoPatrulha = this.ciclo.patrulhaArray[i];
       if (patrulha.id == navParams.data.idPatrulha) {
         this.slideInicial = Number(i);
         break;
@@ -41,7 +42,7 @@ export class CicloDetalhePage {
   }
 
   refresh(evt) {
-    console.log("REFRESH - Begin",  evt);
+    console.log("REFRESH - Begin", evt);
     this.patrulaProvider.load();
     this.torneioProvider.reload();
 
@@ -52,11 +53,81 @@ export class CicloDetalhePage {
     });
   }
 
-  setGrupo(grupo:string) {
+  setGrupo(grupo: string) {
     this.subGrupo = grupo;
   }
 
   help(evt) {
     this.navCtrl.push('HelpPage');
+  }
+
+  helpItem(evt, item: string) {
+    // console.log("HELP: " + item);
+
+    let msg: string;
+
+    switch (item) {
+      case 'pontualidade':
+        msg = "Pontualidade: Pergunte aos chefes...";
+        break;
+
+      case 'presenca':
+        msg = "Presença: Perde 1 ponto por elemento ausente. Começa com 10 pontos";
+        break;
+
+      case 'vestuario':
+        msg = "Inspeção: Pergunte aos chefes...";
+        break;
+
+      case 'participacao':
+        msg = "Participação:  Pergunte aos chefes...";
+        break;
+
+      case 'espiritoEscoteiro':
+        msg = "Espírito Escoteiro:  Pergunte aos chefes...";
+        break;
+
+      case 'jogoTecnico':
+        msg = "Jogo Técnico:  Pergunte aos chefes...";
+        break;
+
+      case 'conquistas':
+        msg = "Conquistas:  Pergunte aos chefes...";
+        break;
+
+      case 'extras':
+        msg = "Extras:  Pergunte aos chefes...";
+        break;
+
+      case 'penalidade':
+        msg = "Penalidade:  Pergunte aos chefes...";
+        break;
+
+      case 'atividadeExterna':
+        msg = "Atividade Externa:  Pergunte aos chefes...";
+        break;
+
+      case 'materialPatrulha':
+        msg = "Material de Patrulha:  Pergunte aos chefes...";
+        break;
+
+      case 'cantoPatrulhaVirtual':
+        msg = "Canto Virtual de Patrulha:  Pergunte aos chefes...";
+        break;
+
+      case 'livrosPatrulha':
+        msg = "Livro de Patrulha:  Pergunte aos chefes...";
+        break;
+
+      default:
+        msg = "HELP: " + item;
+        break;
+    }
+
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000
+    });
+    toast.present();
   }
 }
