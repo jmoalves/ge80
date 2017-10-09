af6d818ebcfa6831fd3cb83aba7753e4c60d4aeb
Joao Mauricio <jmauricio@pobox.com>
2017-10-08 21:53:22 -0300

    Nonce - Evitando cache do browser/app

--- src/components/help-card/help-card.ts
+++ src/components/help-card/help-card.ts
@@ -1,12 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the HelpCardComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
 @Component({
   selector: 'help-card',
   templateUrl: 'help-card.html'
